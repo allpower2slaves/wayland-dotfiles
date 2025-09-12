b380315a430c45dd5fc11ffbd84d920e400ffc97
allpower2slaves <allpower2slaves@users.noreply.github.com>
2025-09-12 17:42:16 +0300

    (install.sh) disabled debug printf

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 __target=""$HOME"/"$(printf "$PATH" | grep -q '\.local/bin' && printf ".local/bin" || printf 'bin')""
 __repo="$(realpath "$(dirname "$0")")"
 
-printf "$__target\n" 1>&2 # debug
 
 printf "wl-lock wl-screenshot dwlrc startwl\n" | tr ' ' '\n' | while read __file; do
 	ln -svif ""$__repo"/"$__file"" ""$__target"/"$__file""
