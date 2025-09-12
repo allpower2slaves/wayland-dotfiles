fb7132aab2e7b18fccec3bb6ddea37027a66ff28
allpower2slaves <allpower2slaves@users.noreply.github.com>
2025-09-12 17:58:42 +0300

    (install.sh) added directory installation for foot

--- install.sh
+++ install.sh
@@ -2,7 +2,15 @@
 __target=""$HOME"/"$(printf "$PATH" | grep -q '\.local/bin' && printf ".local/bin" || printf 'bin')""
 __repo="$(realpath "$(dirname "$0")")"
 
+__configdir="$HOME/.config" 
+
+mkdir -p "$__configdir"
+printf "DEBUG $__target\n" 1>&2 # debug
 
 printf "wl-lock wl-screenshot dwlrc startwl\n" | tr ' ' '\n' | while read __file; do
 	ln -svif ""$__repo"/"$__file"" ""$__target"/"$__file""
+done
+
+printf "foot\n" | tr ' ' '\n' | while read __dir; do
+	ln -svif ""$__repo"/"$__dir"" ""$__configdir"/"$__dir""
 done
